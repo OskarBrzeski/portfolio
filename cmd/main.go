fa51ca522de17089f977b6b018f46cbc23cf3328
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2025-01-16 00:54:42 +0000

    initial setup for htmx and go

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Renderer = NewTemplates()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", 0)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
