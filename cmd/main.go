012889c2dbec1599c4ea2a535bc1371b8acc6c27
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2025-01-20 18:06:06 +0000

    small refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,25 @@
 package main
 
 import (
-	"html/template"
-	"io"
+	"ob/utils"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Templates struct {
-	templates *template.Template
-}
-
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func NewTemplates() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
-}
-
 func main() {
 	e := echo.New()
-	e.Renderer = NewTemplates()
+	e.Renderer = utils.NewTemplates()
 	e.Use(middleware.Logger())
 
+	e.Static("/scripts", "scripts")
+
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", 0)
+		return c.Render(200, "index", nil)
+	})
+
+	e.GET("/test", func(c echo.Context) error {
+		return c.Render(200, "success", nil)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
